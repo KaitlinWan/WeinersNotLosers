7f8bd023a99ef17d745b5faa8128a6b1b0bd88b5
Josh Weiner <joshuaweinernyc@gmail.com>
2019-01-10 12:24:01 -0500

    pushing database functions + user authentication

--- v1/app.py
+++ v1/app.py
@@ -11,32 +11,24 @@ from utils import authenticate
 import funcDB
 
 app = Flask(__name__)
-app.secret_key = "hottttttdogggggg"
-
-def getQuote():
-    '''gets quote from favQs api'''
-    response = urlopen('https://favqs.com/api/qotd')
-    r = response.read()
-    d = json.loads(r.decode('utf-8'))
-    return d['quote']['body']
-
-def getBacon():
-    '''gets meat lorem ipsum'''
-    response = urlopen('https://baconipsum.com/api/?type=meat-and-filler&paras=1')
-    r = response.read()
-    d = json.loads(r.decode('utf-8'))
-    return d
-
-def getAdvice():
-    '''gets advice from Advice Slip API'''
-    response = urlopen('https://api.adviceslip.com/advice')
-    r = response.read()
-    d = json.loads(r.decode('utf-8'))
-    return d['slip']['advice']
+app.secret_key = "wereallweinerssometimes"
 
 @app.route('/')
+@app.route('/index')
 def home():
-    return hi
+    return 0
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    return 0
+
+@app.route('/login' , methods=["GET", "POST"])
+def login():
+    return 0
+
+@app.route('/logout', methods=["GET", "POST"])
+def logout():
+    return 0
 
 if __name__ == '__main__':
     app.debug = True
