694c7636d4a43c300324c79fe986c8bd62342b7e
Kyle Tau <kyletau2@gmail.com>
2019-01-10 20:59:15 -0500

    text methods and turning text into list

--- v1/app.py
+++ v1/app.py
@@ -13,6 +13,30 @@ import funcDB
 app = Flask(__name__)
 app.secret_key = "wereallweinerssometimes"
 
+def getQuote():
+    '''gets quote from favQs api'''
+    response = urlopen('https://favqs.com/api/qotd')
+    r = response.read()
+    d = json.loads(r.decode('utf-8'))
+    return d['quote']['body']
+
+def getBacon():
+    '''gets meat lorem ipsum'''
+    response = urlopen('https://baconipsum.com/api/?type=meat-and-filler&paras=1')
+    r = response.read()
+    d = json.loads(r.decode('utf-8'))
+    return d
+
+def getAdvice():
+    '''gets advice from Advice Slip API'''
+    response = urlopen('https://api.adviceslip.com/advice')
+    r = response.read()
+    d = json.loads(r.decode('utf-8'))
+    return d['slip']['advice']
+
+def wordList(x):
+    return x.split()
+
 @app.route('/')
 @app.route('/index')
 def home():
